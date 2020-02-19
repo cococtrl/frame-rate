9fb630baa687d8039c36b6dd15f68a0149a7c75a
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2020-02-18 20:06:03 -0500

    removed boilerplate and added navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = (props) => {
+    return (
+        <nav>
+            <h1>Frame Rate</h1>
+            <ul>
+                <li>Films</li>
+                <li>Login</li>
+                <li>Signup</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Navbar;
