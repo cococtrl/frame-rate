78391bc22aab0c2f6bb9fb2289c0872756ab8732
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2020-02-19 11:49:32 -0500

    navbar updated and created footer component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from './Navbar.module.css';
 
 const Navbar = (props) => {
     return (
-        <nav>
+        <nav className={styles.navbar}>
             <h1>Frame Rate</h1>
             <ul>
                 <li>Films</li>
