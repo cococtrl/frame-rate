f95141ae84e88d738282b4df864e1ffe6e6fb379
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2020-02-19 12:11:23 -0500

    css updates to footer and navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App-outer-container">
       <Navbar />
       <div className="App-inner-container">
-        
+
       </div>
+      <Footer />
     </div>
   );
 }
