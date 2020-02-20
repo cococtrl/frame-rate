e99cd27475261adf3358f2cb19b2bd69cbb6577e
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2020-02-19 20:35:13 -0500

    form style update

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -2,22 +2,60 @@ import React, { Component } from 'react';
 import styles from './SignupForm.module.css';
 
 class SignupForm extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    };
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
     render () {
         return (
-            <form>
+            <form className={styles.form}>
                 <fieldset>
                     <legend>Signup Form</legend>
                     <label htmlFor="name">Full Name</label>
-                    <input id="name" name="name" type="text"/>
+                    <input 
+                        id="name" 
+                        name="name" 
+                        type="text" 
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
 
                     <label htmlFor="email">Email</label>
-                    <input id="email" name="email" type="email"/>
+                    <input 
+                        id="email" 
+                        name="email" 
+                        type="email" 
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
 
                     <label htmlFor="password">Password</label>
-                    <input id="password" name="password" type="password"/>
+                    <input 
+                        id="password" 
+                        name="password" 
+                        type="password" 
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
 
-                    <label htmlFor="passwordConfirmation">Confirm Password</label>
-                    <input id="passwordConfirmation" name="passwordConfirmation" type="password"/>
+                    <label htmlFor="passwordConfirm">Confirm Password</label>
+                    <input 
+                        id="passwordConfirm" 
+                        name="passwordConfirmation" 
+                        type="password" 
+                        value={this.state.passwordConfirm}
+                        onChange={this.handleChange}
+                    />
                     <button type="submit">Submit</button>
                 </fieldset>
             </form>
