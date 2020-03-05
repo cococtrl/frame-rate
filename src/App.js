e91a2d3a181d374cd9489174b8f052ae06178466
Courtney Coleman <courtcoleman@Courtneys-iMac.local>
2020-03-05 13:18:29 -0500

    sign up functionality updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import {Switch, Route, Router} from 'react-router-dom';
+import React, { Component } from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 import './App.css';
+import userService from './utils/userService';
+
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
@@ -10,29 +12,49 @@ import Films from './pages/Films/Films';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 
-function App() {
-  return (
-    <div className="App-outer-container">
-      <Navbar />
-      <div className="App-inner-container">
-        <Switch>
-          <Route exact path="/" render={props =>
-            <Home />
+class App extends Component {
+
+  state = {
+    user: userService.getUser()
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({ user: userService.getUser() })
+  }
+
+  handleLogout = () => {
+    userService.Logout();
+    this.setState({ user: null });
+  }
+
+  render() {
+    return (
+      <div className="App-outer-container">
+        <Navbar handleLogout={this.handleLogout} />
+        <div className="App-inner-container">
+          <Switch>
+            <Route exact path="/" render={props =>
+              <Home />
+            }/>
+            <Route exact path="/films" render={props =>
+            userService.getUser()
+            ? <Films />
+            : <Redirect to="/login" />
           }/>
-          <Route exact path="/films" render={props =>
-          <Films />
-        }/>
-          <Route exact path="/login" render={props =>
-          <Login />
-        }/>
-          <Route exact path="/signup" render={props =>
-          <Signup />
-        }/>
-        </Switch>
+            <Route exact path="/login" render={props =>
+            <Login />
+          }/>
+            <Route exact path="/signup" render={props =>
+            <Signup 
+            {...props} 
+            handleSignupOrLogin={this.handleSignupOrLogin} />
+          }/>
+          </Switch>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
